e9f258bc55f3326b6e5089f35cd13c10ff16b722
Francesco Veneziano Broccia <francescovb97@gmail.com>
2023-09-05 12:47:32 +0200

    Aggiungta import per database

--- src/app.mjs
+++ src/app.mjs
@@ -1,7 +1,13 @@
-export const lambdaHandler = async (event, context) => {
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+export const lambdaHandler = async (event) => {
     try {
+        console.log(docClient);
         console.log(event);
-        console.log(context);
     } catch (err) {
         console.log(err);
         return err;
